17e9b8e81816587c9a26edcd579786abf5028acb
Leandro <leandrooodesousa@gmail.com>
2019-09-05 17:27:15 -0400

    Created project

---
+++ Signos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Leandro de Sousa Silva on 05/09/19.
+//  Copyright © 2019 AcademyMistic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
